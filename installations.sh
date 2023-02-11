828c16ade794e208c75c0b11998ec49c155e2dd3
paradination <jjy2k4life@yahoo.com>
2023-02-10 20:59:50 -0600

    installation info

--- installations.sh
+++ installations.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
-# Installing Jenkins
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo apt-get update
 
 # Installing Git
-sudo yum install git -y
+sudo apt install git -y
 
-
-#nodejs will be installed in the jenkins server
-# terraform is installed in jenkins server
+#install nodejs + npm node16
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
 
 #install docker
-sudo yum install docker -y
+sudo apt install docker.io -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-
-sudo systemctl start docker
-sudo systemctl enable docker
-
-sudo reboot
+systemctl start docker
+systemctl enable docker
